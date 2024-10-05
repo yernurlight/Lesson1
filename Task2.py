b01c6f97d5036a8d82090c63126aa66641134dee
yernurlight <baidullaev.ernur.007@gmail.com>
2024-10-05 09:33:46 +0500

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,10 @@
+## задание1
+number1 = float(input("введите первое число: "))
+number2 = float(input("введите второе число: "))
+number3 = float(input("введите третье число: "))
+number4 = float(input("введите четвертое число: "))
+number5 = float(input("введите пятое число: "))
+
+average = (number1 + number2 + number3 + number4 + number5) / 5
+
+print(f"среднее арифметическое: {average}")
