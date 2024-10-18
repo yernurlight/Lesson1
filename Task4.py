c6c81cfdb65d429618f71f459c81430661918dc3
yernurlight <baidullaev.ernur.007@gmail.com>
2024-10-18 20:22:04 +0500

    Add files via upload

---
+++ Task4.py
@@ -0,0 +1,21 @@
+# 1 task
+
+def greet(name):
+    print(f"Привет, {name}!")
+
+greet("Ернур")
+
+## 2 task
+
+def proizvedenie(a, b):
+    return a * b
+
+result = proizvedenie(5, 3)
+print(result)
+
+## 3 task
+def sred_arif(a, b, c):
+    return (a + b + c) / 3
+
+result = sred_arif(4, 8, 12)
+print(result)
